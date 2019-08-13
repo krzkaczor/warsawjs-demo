3c607662788982c844bca08a54e53d30de956111
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-08-14 00:53:23 +0200

    init 🔥

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
 
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="app-logo"/>
-
         <p>Hello WarsawJS!</p>
-
-        <button onClick={onClickHandler}>Click me!</button>
       </header>
     </div>
   );
-}
-
-function onClickHandler() {
-  alert("warsaw js!");
 }
 
 export default App;
