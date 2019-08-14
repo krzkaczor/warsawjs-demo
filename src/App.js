bd4a027aecb7867bac7391faad3abd10e65e79eb
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-08-14 19:38:55 +0200

    fix lighthouse + vis reg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="app-logo"/>
+        <img src={logo} className="app-logo" alt="logo" />
         <p>Hello WarsawJS!</p>
       </header>
     </div>
